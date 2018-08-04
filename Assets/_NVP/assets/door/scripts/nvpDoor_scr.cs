78f1a1da2f06c38a2b2f406b6eac008d16870a5f
brubow@newvisionsproject.de <brubow@newvisionsproject.de>
2018-08-04 12:19:33 +0200

    Imported package
    Imported Package

---
+++ Assets/_NVP/assets/door/scripts/nvpDoor_scr.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class nvpDoor_scr : MonoBehaviour {
+
+	// +++ fields +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+	[SerializeField] private bool _open;
+	[SerializeField] private bool _opened;
+	[SerializeField] private Animator _doorAnimator;
+	public GameObject doorIndicator;
+
+
+	// +++ unity callbacks ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+	void Start () {
+		
+	}
+
+	void Update () {
+		if(_open != _opened){
+			_opened = _open;
+			_doorAnimator.SetBool("open", _open);
+		}
+	}
+
+
+	// +++ event handler ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+	void OnTriggerEnter(Collider other)
+	{
+		Debug.Log(other.name);
+		if(other.tag == "Player"){
+
+			_open = true;
+		}
+	}
+
+	void OnTriggerExit(Collider ohter){
+		if(ohter.tag == "Player"){
+			_open = false;
+		}
+	}
+
+
+}
